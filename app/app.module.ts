62f80403c08bffc7e1caedfb3f90e1ec00f618e4
decocas <37914111+decocas@users.noreply.github.com>
2018-09-14 22:18:05 -0500

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FrutaComponent} from './fruta/fruta.component'; 
+ 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FrutaComponent,
+		
+      ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
